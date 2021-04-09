3a8fd993d01806ffcf46d2cb840561afb5554b54
OdongAlican <sandieo.2020@gmail.com>
2021-04-09 10:35:49 +0300

    update Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../actions/individualCustomer';
+import Navbar from './Navbar';
 const Home = () => {
-  return (
-    <div className="App">
-      <p>Home Section</p>
-    </div>
-  );
-}
+
+    const dispatch = useDispatch();
+    const individualCustomers = useSelector(state => state.individualCustomersReducer);
+
+    useEffect(()=> {
+        dispatch(fetchPosts());
+    }, []);
+
+    console.log(individualCustomers.individualCustomers, 'inside data');
+    return(
+        <div className="home-section">
+            <Navbar/>
+            <p>Home section</p>
+        </div>
+    )
+};
 
 export default Home;
