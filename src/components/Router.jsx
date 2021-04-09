824df4ced1d424c20ac0e62559b27a6a500d8761
OdongAlican <sandieo.2020@gmail.com>
2021-04-09 10:39:05 +0300

    add Router component

---
+++ src/components/Router.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import IndividualCustomerForm from './IndividualCustomer';
+
+const Router = () => {
+  return (
+    <div className="router-section">
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/individualcustomerform" component={IndividualCustomerForm} />
+          </Switch>
+        </BrowserRouter>
+    </div>
+  );
+};
+
+export default Router;
