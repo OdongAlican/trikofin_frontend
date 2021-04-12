257679909edeab469e1501508b6f6c028b3783cb
OdongAlican <sandieo.2020@gmail.com>
2021-04-12 16:09:23 +0300

    add UseForm component

---
+++ src/components/UseForm.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { postIndividualCustomers } from '../actions/individualCustomer';
+
+const UseForm = validate => {
+  const [values, setValues] = useState({
+    surname: '',
+    forenameone: '',
+    forenametwo: null,
+    forenamethree: null,
+    residential: '',
+    emailone: null,
+    emailtwo: null,
+    phoneone: null,
+    phonetwo: null,
+    phonethree: null,
+    birthday: '',
+    gender: '',
+    nationality: '',
+    status: '',
+    profile: '',
+    customer: '',
+    title: '',
+  });
+
+  const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setErrors(validate(values));
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0) {
+      dispatch(postIndividualCustomers(values));
+    } else {
+      console.log('sample errors');
+    }
+  }, [errors]);
+
+  return {
+    handleChange, values, handleSubmit, errors,
+  };
+};
+
+export default UseForm;
