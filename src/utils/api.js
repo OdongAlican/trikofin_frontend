9483d1c9e24a8c61286708ca0298816da4656719
OdongAlican <sandieo.2020@gmail.com>
2021-04-12 16:13:51 +0300

    add PostIndividualCustomersRequest method

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,3 +8,8 @@ export const FetchIndividualCustomersRequest = async (method, path) => {
   const response = await axios[method](`${baseUrl}/${path}`);
   return response;
 };
+
+export const PostIndividualCustomersRequest = async (method, data, path) => {
+  const response = await axios[method](`${baseUrl}/${path}`, data);
+  return response;
+};
