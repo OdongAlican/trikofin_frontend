af4f8c8b542269d8bcd314eba838e22bc07d24db
OdongAlican <sandieo.2020@gmail.com>
2021-04-14 20:21:19 +0300

    update routes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import IndividualCustomerForm from './IndividualCustomer';
 import ViewIndividualCustomer from './ViewIndividualCustomer';
+import UpdateIndividualCustomer from './UpdateIndividualCustomer';
 
 const Router = () => (
   <div className="router-section">
@@ -13,6 +14,7 @@ const Router = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/individualcustomerform" component={IndividualCustomerForm} />
+        <Route path="/updatecustomer/:id" component={UpdateIndividualCustomer} />
         <Route
           path="/viewindividualcustomerform/:id"
           render={props => <ViewIndividualCustomer key={props.match.params.id} {...props} />}
