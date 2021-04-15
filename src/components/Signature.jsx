f7824adbf627626cd7d4eb577b8c3b529e11c3a3
codemasteric <onenfelix90@gmail.com>
2021-04-15 07:19:56 +0300

    Added Signature Page

---
+++ src/components/Signature.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import UserAcces from '../images/bank.svg';
+
+const Signature = () => (
+  <>
+    <Navbar />
+    <div className="lower-form-section">
+
+      <div className="lower-downer-section">
+        <div className="left-inner-form-section">
+          <Sidebar />
+        </div>
+        <div className="submit-form-top-section new-height">
+          <div className="maintenance-customer-info">
+            <span>Customer Maintenance Personal Information</span>
+          </div>
+          <div className="welcome-page">
+            <h1>Signature</h1>
+          </div>
+          <div className="banking-logo">
+            <img src={UserAcces} alt="Access User Portal" />
+            <Link to="/individualcustomerform">Go Back Home</Link>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </>
+);
+
+export default Signature;
